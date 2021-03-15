3aabe9af65a5b456a869b8f451667efcf82d858f
MariaPolyniak <masha.poliniak@gmail.com>
2021-03-15 16:15:02 +0200

    add animation

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,18 +1,27 @@
 import React from "react";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 import TodoItem from "./TodoItem";
 
 function TodoList({ todos, onRemoveTodo, onToggleStatus }) {
   return (
     <div className="TodoList">
-      {todos.map((todo, index) => (
-        <TodoItem
-          key={todo.id}
-          todo={todo}
-          index={index}
-          onRemoveTodo={onRemoveTodo}
-          onStatusChange={onToggleStatus}
-        />
-      ))}
+      <TransitionGroup>
+        {todos.map((todo, index) => (
+          <CSSTransition
+            key={todo.id}
+            timeout={500}
+            classNames="item"
+            unmountOnExit
+          >
+            <TodoItem
+              todo={todo}
+              index={index}
+              onRemoveTodo={onRemoveTodo}
+              onStatusChange={onToggleStatus}
+            />
+          </CSSTransition>
+        ))}
+      </TransitionGroup>
     </div>
   );
 }
